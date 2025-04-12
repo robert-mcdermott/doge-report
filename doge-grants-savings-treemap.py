f5ba9b1dbd93026ea40ab5553b7bfd25ceccfb0c
Robert McDermott <robert.c.mcdermott@gmail.com>
2025-04-11 23:15:06 -0700

    updated treemap

--- doge-grants-savings-treemap.py
+++ doge-grants-savings-treemap.py
@@ -16,18 +16,29 @@ df = df[df['savings'] > 0]
 # Create a unique savings entry label
 df["savings_entry"] = df["date"].astype(str) + " | $" + df["savings"].round(0).astype(int).astype(str)
 
+# Compute 95th percentile of savings (or 90th if you want it more aggressive)
+cap = df['savings'].quantile(1.0)
+
 # Create the treemap
 fig = px.treemap(
     df,
     path=["agency", "recipient", "savings_entry"],
     values="savings",
-    title="Drill-down Treemap: Agency → Recipient → Savings Entry",
+    title="<b>DOGE Canceled Grants</b> - Click to drill-down: Agency → Recipient → Grant - <i>(click top gray bar to reset)</i>",
     color="savings",
+    range_color=[0, cap],
     hover_data={
         "savings": True,
         "description": True,
         "savings_entry": False
     },
+
+    #color_continuous_scale="Cividis",
+    #color_continuous_scale="Viridis",
+    #color_continuous_scale="Rainbow",
+    #color_continuous_scale="Turbo",
+    #color_continuous_scale="Inferno",
+    #color_continuous_scale="Plasma",
     color_continuous_scale=[
         [0.0, "blue"],
         [0.2, "cyan"],
@@ -39,11 +50,8 @@ fig = px.treemap(
 )
 
 fig.update_layout(
-    margin=dict(t=50, l=25, r=25, b=25),
-    coloraxis_colorbar=dict(
-        title="Savings (USD)",
-        tickprefix="$"
-    )
+    margin=dict(t=30, l=25, r=25, b=25),
+    coloraxis_showscale=False
 )
 
 # Save to HTML
